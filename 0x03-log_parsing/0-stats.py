6c130de471b5dcede7bc9752c1286794c6cadd9c
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-13 21:13:13 +0300

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,26 +4,38 @@
 import sys
 
 
-cache = {
+stats = {
         '200': 0, '301': 0, '400': 0, '401': 0,
         '403': 0, '404': 0, '405': 0, '500': 0
         }
-size = 0
+total = 0
 count = 0
 
-if __name__ == "__main__":
+
+def print_stats(stats, total):
+    """print_stats function
+
+    Args:
+        stats (dict): stats
+        total (int): total
+    """
+    print(f"File size: {total}")
+    for key, value in sorted(stats.items()):
+        if value:
+            print(f"{key}: {value}")
+
+
+try:
     for line in sys.stdin:
+        ls = line.split()
+        key, size = ls[-2], int(ls[-1])
+        if key in stats.keys():
+            stats[key] += 1
+        total += size
         count += 1
-        try:
-            data = line.split()
-            status = data[-2]
-            size += int(data[-1])
-            if status in cache:
-                cache[status] += 1
-        except Exception:
-            pass
-        if count % 10 == 0:
-            print("File size: {}".format(size))
-            for key, value in sorted(cache.items()):
-                if value:
-                    print("{}: {}".format(key, value))
+        if count == 10:
+            count = 0
+            print_stats(stats, total)
+except KeyboardInterrupt:
+    print_stats(stats, total)
+    raise
