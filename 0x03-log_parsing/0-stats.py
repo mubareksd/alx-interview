e3631b5fa9987d739ab8544e0820a5497a613732
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-14 15:01:21 +0300

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -12,36 +12,18 @@ total = 0
 count = 0
 
 
-def print_stats():
-    """print_stats function
-
-    Args:
-        stats (dict): stats
-        total (int): total
-    """
-    print("File size: {}".format(total))
-    for key, value in sorted(stats.items()):
-        if value > 0:
-            print("{}: {}".format(key, value))
-
-
 if __name__ == "__main__":
-    try:
-        for line in sys.stdin:
-            size = 0
-            try:
-                data = line.split()
-                status = data[-2]
-                size += int(data[-1])
-                if status in stats:
-                    stats[status] += 1
-            except Exception:
-                pass
-            total += size
-            count += 1
-            if count == 10:
-                print_stats()
-                count = 0
-    except KeyboardInterrupt:
-        print_stats()
-        raise
+    for line in sys.stdin:
+        data = line.split()
+        status = data[-2]
+        size = int(data[-1])
+        if status in stats.keys():
+            stats[status] += 1
+        total += size
+        count += 1
+        if count == 10:
+            print("File size: {}".format(total))
+            for key, value in sorted(stats.items()):
+                if value != 0:
+                    print("{}: {}".format(key, value))
+            count = 0
