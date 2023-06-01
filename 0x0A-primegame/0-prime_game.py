392e9a84808b4b33ac9f99e87daf80c4d8cae6f2
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-06-01 21:17:25 +0300

    upd

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,6 +3,39 @@
 """
 
 
+def isPrime(n):
+    """determine if a number is prime
+
+    Args:
+        n (int): number to check
+
+    Returns:
+        bool: True if n is prime, False
+    """
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0 and i != n:
+            return False
+    return True
+
+
+def primes(n):
+    """return a list of prime numbers
+
+    Args:
+        n (int): number to check
+
+    Returns:
+        list: list of prime numbers
+    """
+    lss = []
+    for i in range(n):
+        if isPrime(i):
+            lss.append(i)
+    return lss
+
+
 def isWinner(x, nums):
     """determine whoo the winner of each game is
 
@@ -13,18 +46,19 @@ def isWinner(x, nums):
     Returns:
         str: name of the player that won the most rounds
     """
-    maria, ben = 0, 0
-    if not nums or x < 1:
+    if x < 1 or nums is None or len(nums) < 1:
         return None
+    ben = 0
+    maria = 0
     for i in range(x):
-        if nums[i] % 2 == 0:
+
+        if primes(nums[i]) == []:
+            ben += 1
+        elif len(primes(nums[i])) % 2 == 0:
+            ben += 1
+        else:
             maria += 1
-            continue
-        ben += 1
+    if ben == maria:
+        return None
 
-    if maria > ben:
-        return 'Maria'
-    if ben > maria:
-        return 'Ben'
-
-    return None
+    return "Maria" if maria > ben else "Ben"
